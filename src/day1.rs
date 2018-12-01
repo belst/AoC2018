2d323fa321d58b12f0cccc6efc239d194940b1bc
belst <git@bel.st>
2018-12-01 18:17:14 +0100

    add generator for better benchmarks

--- src/day1.rs
+++ src/day1.rs
@@ -1,15 +1,23 @@
+#[aoc_generator(day1)]
+pub fn gen(input: &str) -> Vec<i64> {
+    input
+        .lines()
+        .map(str::parse::<i64>)
+        .filter_map(Result::ok)
+        .collect()
+}
+
 #[aoc(day1, part1)]
-pub fn part1(input: &str) -> i64 {
-    input.lines().map(|x| x.parse::<i64>().unwrap()).sum()
+pub fn part1(input: &[i64]) -> i64 {
+    input.iter().sum()
 }
 
 #[aoc(day1, part2)]
-pub fn part2(input: &str) -> i64 {
+pub fn part2(input: &[i64]) -> i64 {
     let mut seen = std::collections::HashSet::new();
 
     input
-        .lines()
-        .map(|x| x.parse::<i64>().unwrap())
+        .iter()
         .cycle()
         .scan(0, |a, c| {
             *a += c;

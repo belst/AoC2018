68c061b7107d65974e4a8b30fd3a14b744dfd0fa
belst <git@bel.st>
2018-12-01 17:36:02 +0100

    remove loop

--- src/day1.rs
+++ src/day1.rs
@@ -7,20 +7,13 @@ pub fn part1(input: &str) -> i64 {
 pub fn part2(input: &str) -> i64 {
     let mut seen = std::collections::HashSet::new();
 
-    let mut it = input
+    input
         .lines()
         .map(|x| x.parse::<i64>().unwrap())
         .cycle()
         .scan(0, |a, c| {
             *a += c;
             Some(*a)
-        });
-
-    loop {
-        let v = it.next().unwrap();
-        if seen.contains(&v) {
-            break v;
-        }
-        seen.insert(v);
-    }
+        }).find(|c| !seen.insert(*c))
+        .unwrap()
 }
